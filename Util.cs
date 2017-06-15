1b85b4a0236380ecd05c78ecd8b24eef4a7170d7
PCY0RPEQODEFG28\Administrator <frank098@sina.com>
2017-06-15 17:39:55 +0800

    第三次我要提交

---
+++ Util.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//写一些公共类型
+/// <summary>
+///装备存储位置类型 
+/// </summary>
+public enum LocationType{
+	//0代表什么也没有。1代表bag有装备没有。2代表装备栏有背包没有。3代表所有都有
+	Null=0,
+	Bag,
+	Equip,
+	All
+}
+
+/// <summary>
+/// 玩家人物属性4个类型
+/// </summary>
+public enum PropertyType
+{
+	AD,
+	AP,
+	Armor,
+	Magic
+}
+
+/// <summary>
+/// 装备属性类型
+/// </summary>
+public enum EquipType
+{
+	AD,
+	AP,
+	Armor,
+	Magic,
+	Hemophagia,
+	ReduceCD
+}
+
+
+public class Util : MonoBehaviour {
+
+
+}
